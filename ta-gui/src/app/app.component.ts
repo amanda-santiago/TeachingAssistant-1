d0fbec4d9e88f48ddc3ff752f299488a615df811
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    adicionei o logingithub no formulario

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,5 +1,9 @@
+
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
+
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +12,10 @@ import { NgModule } from '@angular/core';
 })
 export class AppComponent {
    aluno: Aluno = {nome: "", cpf: "", email: "", loginGitHub:""};
-}
+   alunoService = new AlunoService();
 
-export class Aluno {
-  nome: string = "";
-  cpf: string = "";
-  email: string = "";
-  loginGitHub: string = "";
-
+   gravar(a: Aluno): void {
+     this.alunoService.gravar(a);
+     this.aluno = {nome: "", cpf: "", email: "", loginGitHub:""};
+  }
 }
